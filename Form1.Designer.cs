8ec82998f9f14bdea63ca579aacd770c8a8d7294
nuntawat <nanthawat.s@kkumail.com>
2023-03-05 08:03:58 +0700

    A

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,12 +28,179 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbname = new System.Windows.Forms.TextBox();
+            this.tbstudent = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.Aj = new System.Windows.Forms.Button();
+            this.tblist = new System.Windows.Forms.TextBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.groupBox1.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(21, 47);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 99);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(96, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Student ID";
+            // 
+            // tbname
+            // 
+            this.tbname.Location = new System.Drawing.Point(114, 44);
+            this.tbname.Name = "tbname";
+            this.tbname.Size = new System.Drawing.Size(143, 31);
+            this.tbname.TabIndex = 2;
+            // 
+            // tbstudent
+            // 
+            this.tbstudent.Location = new System.Drawing.Point(114, 96);
+            this.tbstudent.Name = "tbstudent";
+            this.tbstudent.Size = new System.Drawing.Size(143, 31);
+            this.tbstudent.TabIndex = 3;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.Aj);
+            this.groupBox1.Controls.Add(this.tbstudent);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.tbname);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Location = new System.Drawing.Point(12, 36);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(293, 221);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Infermation";
+            // 
+            // Aj
+            // 
+            this.Aj.Location = new System.Drawing.Point(114, 133);
+            this.Aj.Name = "Aj";
+            this.Aj.Size = new System.Drawing.Size(143, 41);
+            this.Aj.TabIndex = 4;
+            this.Aj.Text = "Add";
+            this.Aj.UseVisualStyleBackColor = true;
+            this.Aj.Click += new System.EventHandler(this.Aj_Click);
+            // 
+            // tblist
+            // 
+            this.tblist.Location = new System.Drawing.Point(382, 12);
+            this.tblist.Multiline = true;
+            this.tblist.Name = "tblist";
+            this.tblist.Size = new System.Drawing.Size(392, 389);
+            this.tblist.TabIndex = 5;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 33);
+            this.menuStrip1.TabIndex = 6;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.saveToolStripMenuItem,
+            this.openToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(54, 29);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(270, 34);
+            this.saveToolStripMenuItem.Text = "save";
+            this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(270, 34);
+            this.openToolStripMenuItem.Text = "open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(270, 34);
+            this.exitToolStripMenuItem.Text = "Exit";
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutToolStripMenuItem});
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(65, 29);
+            this.helpToolStripMenuItem.Text = "Help";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(169, 34);
+            this.aboutToolStripMenuItem.Text = "About ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tblist);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private Label label1;
+        private Label label2;
+        private TextBox tbname;
+        private TextBox tbstudent;
+        private GroupBox groupBox1;
+        private Button Aj;
+        private TextBox tblist;
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem fileToolStripMenuItem;
+        private ToolStripMenuItem saveToolStripMenuItem;
+        private ToolStripMenuItem exitToolStripMenuItem;
+        private ToolStripMenuItem helpToolStripMenuItem;
+        private ToolStripMenuItem aboutToolStripMenuItem;
+        private ToolStripMenuItem openToolStripMenuItem;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -1,10 +1,65 @@
+using StudentClassLibrary;
+using System.Runtime.CompilerServices;
+
 namespace MMG
 {
     public partial class Form1 : Form
     {
+        StudentController controller = new StudentController();
+        /*List<Student> listStudents = new List<Student>();*/
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Aj_Click(object sender, EventArgs e)
+        {
+            this.tblist.Text = "";
+            string input_name = this.tbname.Text;
+            string input_Student_id = this.tbstudent.Text;
+            controller.addStudent2List(input_Student_id , input_name);
+            displayData();
+
+
+            /*Student newStudent = new Student(input_name, input_Student_id);
+            this.listStudents.Add(newStudent);
+            for(int i = 0 ; i < this.listStudents.Count; i++)
+            {
+                Student currentStudent = this.listStudents[i];
+                string display = currentStudent.displayInfo();
+                this.tblist.Text += display;
+                this.tbname.Text = "";
+                this.tbstudent.Text = "";
+            }*/
+        }
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            controller.saveFile();
+
+            /*string save_data = this.tblist.Text;
+            File.WriteAllText("d:/text.csv" , save_data);*/
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            controller.openFile();
+            displayData();
+
+            /*string[] data = File.ReadAllLines("d:/text.csv");
+
+            //[ ] Add data to listStudents
+            for(int i = 0; i < data.Length; i++)
+            {
+                string idata = data[i];
+                //add idata to listStudent
+                //using SubString
+            }
+            // [ / ] Display data to tblistofStudent*/
+        }
+        private void displayData()
+        {
+            this.tblist.Text = controller.displayData();
         }
     }
 }
